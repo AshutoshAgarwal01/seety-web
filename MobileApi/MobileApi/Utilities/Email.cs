502fa5c246730e8493cc839ad5904463fb8e6086
Ashutosh Agarwal <ashutosh.jaipur@gmail.com>
2018-05-24 00:15:24 -0700

    Added email template

--- MobileApi/MobileApi/Utilities/Email.cs
+++ MobileApi/MobileApi/Utilities/Email.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
 
 namespace MobileApi.Utilities
 {
@@ -12,16 +15,18 @@ namespace MobileApi.Utilities
         static bool enableSSL = true;
         static string emailFromAddress = "agarwala.uw@gmail.com"; //Sender Email Address  
         static string password = ConfigurationManager.AppSettings["password"]; //"Abc@123$%^"; //Sender Password  
-        static string subject = "Hello";
+        static string subject = "Order received!";
 
         public static void SendEMail(string recipient, string subject, string body)
         {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"EmailHtml\BasicMor.html");
+            string emailHtml = File.ReadAllText(path);
             using (MailMessage mail = new MailMessage())
             {
                 mail.From = new MailAddress(emailFromAddress);
                 mail.To.Add(recipient);
                 mail.Subject = subject;
-                mail.Body = body;
+                mail.Body = emailHtml;
                 mail.IsBodyHtml = true;
                 //mail.Attachments.Add(new Attachment("D:\\TestFile.txt"));//--Uncomment this to send any attachment  
                 using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
